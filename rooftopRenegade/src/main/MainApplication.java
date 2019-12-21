991c6db930e2131515804901daeb6dd1542c20fa
leonard <leonard@192.168.0.12>
2019-12-20 23:17:41 -0500

    Learning to use things

--- rooftopRenegade/src/main/MainApplication.java
+++ rooftopRenegade/src/main/MainApplication.java
@@ -4,6 +4,12 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -11,15 +17,34 @@ import javafx.stage.Stage;
  *
  */
 public class MainApplication extends Application {
-
+	
 	/* (non-Javadoc)
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	@Override
 	public void start(Stage mainWindow) throws Exception {
 		mainWindow.setTitle("Rooftop Renegade");
-		mainWindow.show();
+		
+		mainWindow.setResizable(false);
+		mainWindow.setMaxWidth(800);
+		mainWindow.setMinWidth(800);
+		mainWindow.setMaxHeight(600);
+		mainWindow.setMinHeight(600);
+		
+		Canvas canvas = new Canvas(800, 600);
+		final GraphicsContext gc = canvas.getGraphicsContext2D();
+		canvas.setFocusTraversable(true);
+		
+		draw(gc);
+		
 
+		//Group group = new Group();
+		StackPane layout = new StackPane();
+		layout.getChildren().add(canvas);
+
+		Scene scene = new Scene(layout, 300, 250);
+		mainWindow.setScene(scene);
+		mainWindow.show();
 	}
 
 	/**
@@ -27,6 +52,19 @@ public class MainApplication extends Application {
 	 */
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	/**
+	 * Display basic information from mouse listeners
+	 * @param g The graphics context
+	 **/
+	public void draw (GraphicsContext gc){
+		gc.setFill(Color.WHITE);
+		gc.fillRect(0, 0, 800, 600);
+		
+		gc.setFill(Color.BLACK);
+		gc.fillRect(100, 100, 30, 50);
+
 
 	}
 
