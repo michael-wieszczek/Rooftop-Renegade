fc644c2a49fec129233249b974f97f325ade17a2
mimlo <mimlo@LAPTOP-FTRFLFR3>
2019-12-23 19:59:31 -0500

    Basic Title Screen with buttons

--- rooftopRenegade/src/main/MainApplication.java
+++ rooftopRenegade/src/main/MainApplication.java
@@ -4,6 +4,10 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -17,7 +21,22 @@ public class MainApplication extends Application {
 	 */
 	@Override
 	public void start(Stage mainWindow) throws Exception {
+		Button game = new Button("Start");
+		Button settings = new Button("Settings");
+		Button exit = new Button("Exit");
+		game.setOnAction(e -> System.out.println("Placeholder"));
+		settings.setOnAction(e -> System.out.println("Placeholder"));
+		exit.setOnAction(e -> {
+			//Save Leaderboards
+			System.exit(0);
+		});
+		VBox layout = new VBox();
+		layout.getChildren().addAll(game, settings, exit);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setCenter(layout);
+		Scene scene = new Scene(borderPane, 500, 300);
 		mainWindow.setTitle("Rooftop Renegade");
+		mainWindow.setScene(scene);
 		mainWindow.show();
 
 	}
