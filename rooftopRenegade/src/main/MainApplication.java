705ec910efd149a32d9cbe4137c2d3ed708f73f5
335203600 <335203600@C-4028YR1.tdsb.on.ca>
2020-01-09 11:01:00 -0500

    Made classes for platform and player

--- rooftopRenegade/src/main/MainApplication.java
+++ rooftopRenegade/src/main/MainApplication.java
@@ -3,13 +3,14 @@
  */
 package main;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.geometry.Point2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -18,67 +19,68 @@ import javafx.stage.Stage;
  */
 public class MainApplication extends Application {
 	
-	/* (non-Javadoc)
-	 * @see javafx.application.Application#start(javafx.stage.Stage)
-	 */
-	@Override
-	public void start(Stage mainWindow) throws Exception {
 
-		Button game = new Button("Start");
-		game.setLayoutX(380);
-		game.setLayoutY(250);
-		
-		Button settings = new Button("Settings");
-		settings.setLayoutX(380);
-		settings.setLayoutY(275);
-		
-		Button exit = new Button("Exit");
-		exit.setLayoutX(380);
-		exit.setLayoutY(300);
-		
-		game.setOnAction(e -> {
-			//Placeholder Code
-		});
-		settings.setOnAction(e -> {
-			//Placeholder Code
-		});
-		exit.setOnAction(e -> {
-			//Save Leaderboards
-			Platform.exit();
-		});
-		
-		AnchorPane layout = new AnchorPane();
-		layout.getChildren().addAll(game, settings, exit);
-		BorderPane borderPane = new BorderPane();
-		borderPane.setCenter(layout);
-		Scene scene = new Scene(borderPane, 800, 500);
-		
-		mainWindow.setTitle("Rooftop Renegade");
-		mainWindow.setResizable(false);
-		mainWindow.setMaxWidth(800);
-		mainWindow.setMinWidth(800);
-		mainWindow.setMaxHeight(500);
-		mainWindow.setMinHeight(500);
-		
-		mainWindow.setScene(scene);
-		mainWindow.show();
+	private Pane root = new Pane();
+	
+	private Player player = new Player(300, 280, 40, 60, "player", Color.BLUEVIOLET);
+	Platform s = new Platform(0, 340, 800, 5, 340, "platform", Color.BLACK);
+	Platform s2 = new Platform(0, 240, 500, 5, 240, "platform", Color.BLACK);
+	private Point2D playerVelocity = new Point2D(0, 0);
+	private boolean canJump = true;
 
+	private Parent initGame() {
+		root.setPrefSize(800, 600);
+		
+		root.getChildren().add(player);
+		
+		AnimationTimer timer = new AnimationTimer() {
+			
+			public void handle(long now) {
+				update();
+				System.out.println(System.currentTimeMillis());
+			}
+		};
+		
+		timer.start();
+		
+		Platform();
+		
+		return root;
+	}
+	
+	private void Platform() {
+		
+		root.getChildren().addAll(s, s2);
 	}
 
-	/**
-	 * @param args
-	 */
+	private void update() {
+		
+	}
+	
+	public void start (Stage stage) throws Exception {
+		Scene scene = new Scene(initGame());
+		
+		scene.setOnKeyPressed(e -> {
+			if(e.getCode() == KeyCode.W || e.getCode() == KeyCode.UP) {
+				player.jump();
+			}
+			else if(e.getCode() == KeyCode.S || e.getCode() == KeyCode.DOWN) {
+				if(player.getBoundsInParent().intersects(s.getBoundsInParent()) || player.getBoundsInParent().intersects(s2.getBoundsInParent())) {
+					boolean canJump = true;
+				}
+				else {
+					player.tempGravity();
+				}
+				
+			}
+				
+		});
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
-
-	/**
-	 * Displays the basic game information
-	 * @param g The graphics context
-	 **/
-	public void draw (GraphicsContext gc){
-
-
-	}
-
 }
---
+++ rooftopRenegade/src/main/Platform.java
@@ -0,0 +1,24 @@
+package main;
+
+import javafx.scene.paint.Color;
+
+public class Platform extends Sprite{
+
+	double xSpeed;
+	int top;
+	
+	Platform(int x, int y, int w, int h, int top, String type, Color color) {
+		super(x, y, w, h, type, color);
+		
+		this.top = top;
+		this.xSpeed = 1;
+
+	}
+	
+	public int getTop(){
+		return this.top;
+		
+	}
+
+	
+}
